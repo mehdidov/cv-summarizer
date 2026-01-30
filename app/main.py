fd7524dff59a5a90928b2ba12902c6c2a0a0871c
mehdidov <125817777+mehdidov@users.noreply.github.com>
2026-01-30 11:50:46 +0100

    Content type validation

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.core.route import router as core_router
+from app.routers.cvs import router as cvs_router
 
 app = FastAPI(title="cv-summarize")
 
 app.include_router(core_router)
+app.include_router(cvs_router)
--- app/routers/cvs.py
+++ app/routers/cvs.py
@@ -1,12 +1,17 @@
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, UploadFile, File, HTTPException
 
 router = APIRouter(
     prefix="/cvs",
     tags=["cvs"]
 )
 
-router.post("/upload")
+@router.post("/upload")
 async def cv_upload(file: UploadFile = File(...)):
+    if file.content_type != "application/pdf":
+        raise HTTPException(
+            status_code = 415,
+            detail = "Unsupported Media Type"
+        )
     return {
         "filename": file.filename
     
